5989626235dec11943f4521ed986a189565644a9
chebeto <chebeto.sanchez@gmail.com>
2018-09-25 21:39:33 -0500

    Adicionada la BD de Firebase y su conexion

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,13 +10,28 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor( private http: HttpClient) { 
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe((resp: InfoPagina) => {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-      });
+  equipo: any[] = [];
 
+  constructor( private http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
   }
+
+  private cargarInfo(){
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe((resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-9a4bf.firebaseio.com/equipo.json')
+    .subscribe((resp: any)  => {
+      this.cargada = true;
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
 }
