3a7a43ed46dc1fa7321211bab492da840f8ca110
chebeto <chebeto.sanchez@gmail.com>
2018-09-27 19:01:34 -0500

    Servicios agregados

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
+  cargando = true;
+
   constructor(private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-9a4bf.firebaseio.com/productos_idx')
-    .subscribe( (resp: any[]) => {
+    this.http.get('https://angular-html-9a4bf.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: Producto[]) => {
       console.log(resp);
-      
+
+      this.cargando = false;
     });
   }
 
