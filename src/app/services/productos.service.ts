ae79f439c780237f745b41fac0ef48be558717ed
chebeto <chebeto.sanchez@gmail.com>
2018-09-28 22:48:51 -0500

    Agregados servicios e interface de productos y su descripcion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -17,10 +17,12 @@ export class ProductosService {
   private cargarProductos(){
     this.http.get('https://angular-html-9a4bf.firebaseio.com/productos_idx.json')
     .subscribe( (resp: Producto[]) => {
-      console.log(resp);
       this.productos = resp;
       this.cargando = false;
     });
   }
 
+  getProducto( id: string ){
+    return this.http.get(`https://angular-html-9a4bf.firebaseio.com/productos/${ id }.json`);
+  }
 }
